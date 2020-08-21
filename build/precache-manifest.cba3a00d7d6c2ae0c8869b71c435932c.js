cf409b27de32118d22e91fbbfa73e8c2eae179d2
Solai Raj <msraj085@gmail.com>
2020-08-21 17:19:57 +0530

    adding manual build

---
+++ build/precache-manifest.cba3a00d7d6c2ae0c8869b71c435932c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e51f3873dc272e96b6342a1b90797b5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b34a479712a6f671c97f",
+    "url": "/static/css/main.9472edd8.chunk.css"
+  },
+  {
+    "revision": "03c6637b0d94f71e9caf",
+    "url": "/static/js/2.2bdea00b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2bdea00b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b34a479712a6f671c97f",
+    "url": "/static/js/main.a3d27012.chunk.js"
+  },
+  {
+    "revision": "818fe9f3b582991bac66",
+    "url": "/static/js/runtime-main.c88050f1.js"
+  },
+  {
+    "revision": "b48fb6219553b9d3830dc1dbe8a729d7",
+    "url": "/static/media/Nodata.b48fb621.svg"
+  },
+  {
+    "revision": "33759894c3f716d5b967e3b102d03443",
+    "url": "/static/media/andrillogo.33759894.svg"
+  },
+  {
+    "revision": "5f2dbff933bd990e02ba65a49fb3f6d1",
+    "url": "/static/media/andrillogo.5f2dbff9.png"
+  },
+  {
+    "revision": "47bb86d222a2536b59e004ad88681898",
+    "url": "/static/media/andrillogoop.47bb86d2.svg"
+  },
+  {
+    "revision": "2c0862a67bfb8773d52c7833e17cd5ca",
+    "url": "/static/media/chevron-down-solid.2c0862a6.svg"
+  },
+  {
+    "revision": "5ef46399592700e9a27a31c24d310d37",
+    "url": "/static/media/hand.5ef46399.jpg"
+  },
+  {
+    "revision": "1b14c1aea82b56c12b2e3ee2eae7fc5a",
+    "url": "/static/media/loadingandril.1b14c1ae.svg"
+  },
+  {
+    "revision": "f16782f01cfc79812e7981c432a70458",
+    "url": "/static/media/userpageimg1.f16782f0.svg"
+  },
+  {
+    "revision": "1fecd0f6ac10cff8fb5a3f9011a88bdf",
+    "url": "/static/media/userpageimg2.1fecd0f6.svg"
+  },
+  {
+    "revision": "f8ca2dfb5e799a199928ccd0a654b146",
+    "url": "/static/media/womansittinglaptop.f8ca2dfb.svg"
+  }
+]);
